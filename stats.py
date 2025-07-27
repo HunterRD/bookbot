4e96566ca6bcd318728113f30469aa8ecbcd76f7
HunterRD <HunterRD47@gmail.com>
2025-07-27 15:41:03 -0500

    updated to print a report

--- stats.py
+++ stats.py
@@ -1,13 +1,26 @@
-def count_words(text):
+def get_num_words(text):
     words = text.split()
     return len(words)
 
-def count_characters(text):
-    characters = {}
-    text = text.lower()  # Normalize to lowercase for character count
-    for char in text:
-        if char in characters:
-            characters[char] += 1
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            characters[char] = 1
-    return characters
+            chars[lowered] = 1
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
